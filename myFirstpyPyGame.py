a4e64b0e6c9b0151acd03edc28be33606644557f
s11514007 <70585633+s11514007@users.noreply.github.com>
2021-12-01 13:55:30 -0500

    v0.4

--- myFirstpyPyGame.py
+++ myFirstpyPyGame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Camren Johnson, 11/29/21 1:59pm, v0.0
+# My First PyGame, Camren Johnson, 11/29/21 1:55pm, v0.4
 
 import pygame, sys
 from pygame.locals import * 
@@ -16,3 +16,24 @@ WHITE = (255, 255, 255)
 RED = (225, 0, 0)
 GREEN = (0, 255, 0
 BLUE = (0, 0, 255)
+FLAMNGOPINK = (168, 50, 117)
+
+# Setup font.
+basicfront = [pygame.font.SysFont(None, 48)
+
+# Setup text
+text = basicFont.render('Hello, world!', True, WHITE, BLUE)
+textRect= text.get_rect()
+textRect.centrex = windowsurface.get_rect().centrex
+textRect, centery + windowSurface.get_rect().centery
+
+# Fill background color 
+windowSurface.fill(FLAMINGOPINK)
+
+# Draw a polygon onto the screen.
+pygame.draw.polygon(windowSurface, Green,((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+
+#Draw lines on the screen.
+pygame.draw.line(windowSurface,BLUE, (0,150), (150,0), 1)
+pygame.draw.line(windowSurface,RED, (60,0), (120,0), 4)
+pygame.draw.line(windowSurface,WHITE, (75,60), (60,75), 2)
