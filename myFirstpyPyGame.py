544d368a1cc5daab6733cebdf89d3bd9f65378be
s11514007 <70585633+s11514007@users.noreply.github.com>
2021-12-03 14:02:22 -0500

    v0.9

--- myFirstpyPyGame.py
+++ myFirstpyPyGame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Camren Johnson, 11/29/21 2:15pm, v0.7
+# My First PyGame, Camren Johnson, 11/29/21 2:02pm, v0.9
 
 import pygame, sys
 from pygame.locals import * 
@@ -6,32 +6,33 @@ from pygame.locals import *
 # Start PyGame 
 pygame.init()
 
+
 # Setup our window. 1 
-windowSurface = pygame.set_mode((500,400), 0, 32)
+windowSurface = pygame.display.set_mode((500,400),0, 32)
 pygame.display.set_caption('Hello, world!')
 
 # Setup Colors 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (225, 0, 0)
-GREEN = (0, 255, 0
+GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-FLAMNGOPINK = (168, 50, 117)
+FLAMINGOPINK = (168, 50, 117)
+
 
 # Setup font.
-basicfront = [pygame.font.SysFont(None, 48)
+basicFont = pygame.font.SysFont(None, 48)
 
 # Setup text
 text = basicFont.render('Hello, world!', True, WHITE, BLUE)
 textRect= text.get_rect()
-textRect.centrex = windowsurface.get_rect().centrex
-textRect, centery + windowSurface.get_rect().centery
+textRect.centery + windowSurface.get_rect().centery
 
 # Fill background color 
 windowSurface.fill(FLAMINGOPINK)
 
 # Draw a polygon onto the screen.
-pygame.draw.polygon(windowSurface, Green,((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+pygame.draw.polygon(windowSurface, GREEN,((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
 
 #Draw lines on the screen.
 pygame.draw.line(windowSurface,BLUE, (0,150), (150,0), 1)
@@ -45,18 +46,18 @@ pygame.draw.circle(windowSurface,BLACK, (300, 50), 20, 0)
 pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80),1)
 
 # Draw a rectangle
-pygame.draw.rect(windowSurface, RED, (textRect.left-20, textRect.width + 40, textRect, height + 40,))
+pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
 
 # Create Pixel Array
-pixelArray = pygame.pixelArray(windowSurface)
-pixelArray[480][380] = BLUE 
+pixArray = pygame.PixelArray(windowSurface)
+pixArray[480][380] = BLUE 
 del pixArray
 
 # Draw the text onto the surface.
-windowSurface.Blit(text, textRect
+windowSurface.blit(text, textRect)
 
 #Update Pygame Display
-pygame,display.update()
+pygame.display.update()
 
 # Run game loop.
 while True:
