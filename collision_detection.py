7734a5d95213173203ecbf2827e47d782a9c9f26
s11514007 <70585633+s11514007@users.noreply.github.com>
2022-02-08 20:12:12 -0500

    v0.1
    Imported pygame system

--- collision_detection.py
+++ collision_detection.py
@@ -1,1 +1,8 @@
-# PyGame Collision Detection Practice, Camren Johnson, Feburary 08, 2022, 8:03, v0.0
+# PyGame Collision Detection Practice, Camren Johnson, Feburary 08, 2022, 8:03, v0.1
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init()
+mainClock = pygame.time.Clock()
